7e2596743d728570000c4fee17795250bfe79b88
Fendrir <papao1@hotmail.fr>
2018-02-13 13:12:10 +0100

    mise en place de la recherche par titre

--- src/AppBundle/Controller/RapportController.php
+++ src/AppBundle/Controller/RapportController.php
@@ -11,16 +11,39 @@ use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
+use Symfony\Component\HttpFoundation\Request;
+
+
 
 class RapportController extends Controller
 {
     /**
      * @Route("/rapport")
      */
-    public function AfficherRapAction()
+    public function AfficherRapAction(Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+        $articles = $em->getRepository('AppBundle:Article')->findAll();
+
+        $form = $this->createFormBuilder()
+        ->add('recherche', Searchtype::class, array('required' => false,
+        'label' => ' ',
+        'attr' => array('placeholder' => 'Recherche')))
+        ->getForm();
+        $form->handlerequest($request);
+
+        if($form->isValid() && $form->isSubmitted()){
+            $data = $form->getData();
+            $parameter = $data['recherche'];
+            $query = $em->createQuery("select a from 
+            AppBundle\Entity\Article as a where a.title like :p")
+            ->setParameters(['p' => '%' . $parameter . '%']);
+            $articles = $query->getResult();
+        }
+
         return $this->render('AppBundle:Rapport:afficher_rap.html.twig', array(
-            // ...
+            "articles" => $articles,
+            'form' => $form->createView(),
         ));
     }
 
