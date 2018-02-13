0d7c563ec1f65c49b958f90aebf0135ce7b4082c
Fendrir <papao1@hotmail.fr>
2018-02-13 11:06:45 +0100

    mise en place du projet, des bundles ( vich, easyadmin, fosuserbundle ) // en cours mise en place d'un uploader pdf, à venir controllers et newformtype

---
+++ README.md
@@ -0,0 +1,4 @@
+site_rapport_stage
+==================
+
+A Symfony project created on February 12, 2018, 9:12 am.
---
+++ src/AppBundle/Entity/Administrateur.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * Administrateur
+ *
+ * @ORM\Table(name="adm_administrateur")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\AdministrateurRepository")
+ */
+class Administrateur extends BaseUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="adm_id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="adm_firstName", type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="adm_phoneNumber", type="string", length=255, nullable=true)
+     */
+    private $phoneNumber;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return Administrateur
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set phoneNumber
+     *
+     * @param integer $phoneNumber
+     *
+     * @return Administrateur
+     */
+    public function setPhoneNumber($phoneNumber)
+    {
+        $this->phoneNumber = $phoneNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get phoneNumber
+     *
+     * @return int
+     */
+    public function getPhoneNumber()
+    {
+        return $this->phoneNumber;
+    }
+}
+
